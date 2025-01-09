77f9d303605bee98c60ccd2bae98cc36cd5acaa4
Teresa de Candia <decandia_te@am.adecco.net>
2025-01-09 17:04:13 -0500

    small improvements and color highlighting

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 import google.generativeai as genai
 from get_dir_structure import get_structure_in_target_dir
+import json
 from read_all_files import read_all_files_in_target_dir
 from read_file import read_file_in_target_dir
 from write_file import write_file_in_target_dir
@@ -29,6 +30,8 @@ TOOLNAMES_SUBSET_HUMAN_NOT_REQUIRED = [
     "read_file_in_target_dir",
     ]
 
+MODELS_BY_PREFERENCE = ["gemini-1.5-pro", "gemini-1.0-pro", "gemini-1.5-flash"]
+
 def log_message(message, filepath="interaction_log.txt"):
     try:
         with open(filepath, "a") as f:
@@ -38,7 +41,13 @@ def log_message(message, filepath="interaction_log.txt"):
         print(f"Error logging message: {e}")
         return False
 
+model_index = 0
+ai_reminders = ''
 n_calls = 0
+
+if os.path.exists("ai_reminders.txt"):
+    with open("interaction_log.txt", "r") as f:
+        ai_reminders = f.read()
 
 # Load environment variables from the .env file
 load_dotenv()
@@ -46,34 +55,23 @@ load_dotenv()
 genai.configure(api_key=os.environ['API_KEY'])
 enable_automatic_function_calling = False  # Warning: Do not use `enable_automatic_function_calling=True` in production applications as there are no data input verification checks for automatic function calls.
 
-model = genai.GenerativeModel(model_name='gemini-1.5-pro',  # "gemini-1.5-pro", "gemini-1.5-flash"
-                              tools=TOOLS)
+model = genai.GenerativeModel(model_name=MODELS_BY_PREFERENCE[model_index],
+                              tools=TOOLS)  # TODO: increment model_index and re-instantiate model anytime error is raised in send_message call due to daily token limit: google.api_core.exceptions.ResourceExhausted: 429 Resource has been exhausted (e.g. check quota).
 
 chat = model.start_chat(enable_automatic_function_calling=enable_automatic_function_calling)
-
-# # Example calls:
-# message = "Help me build a toy app that can be used as a 'calculator' that 'adds' and 'subtracts' colors together. \
-#     Use the read_all_files function to see exactly where the project is up to now, and then FOLLOWING THAT intention, use the write_file function to create new files or overwrite existing ones as needed, and use the read_file function to confirm that your files are being written correctly."
-# message = "Can you write a simple web browser implementation (e.g., that uses uvicorn and whatever else is needed) and a requirements.txt file as well as a README."
-# message = "First, try and test each of the API functions you have available to make sure they work. For example, let me know how many files are in the target directory you have access to. \
-#     Then use the available functions (e.g., write_file) to create a toy app that can be used as a 'calculator' that 'adds' and 'subtracts' colors together, including a small front-end UI that can run in the browser. \
-#     You can pretend that you will have access to any environment you need, and in fact you should write a requirements.txt file that includes the libraries you will need, including uvicorn and \
-#     whatever else would be needed to create some basic web browser UI. You should also write a README.py and a simple main.py script."
-# message = """Can you test each of the API functions you have available to make sure each works. Start by looking at the \
-#     target directory structure, then read the README, and then read all the files ..do not test the write_file function yet though."""  # TODO: Later the message should be injected by the user
-# message = "You are helping me build a library that leverage Gemini to facilitate paired programming between an AI and a human (i.e., human-in-the-loop AI programming). Please use the functions at your dissposal to read the README and see the directory structure. After that, use read_file to take a close look at write_file.py and how it imports the TARGET_DIR so that the write_file function within it is limited to accessing only files within the TARGET_DIR. Once you understand how it's done there, please use the read_file and write_file functions to update file_system_opearations.py so that it imports TARGET_DIR and so as so limit the relative_path to be within the TARGET_DIR"
-# message = 'Read the readme to understand the point of the library in the target directory, as well as all the files. You should then test the code to make sure it functions as intended and make suggestions for improvements'
-# message = "Read the readme, test all the functions, and let me know how you would improve the main.py file"
 
 while True:
     message = input("Enter a question for AI (or type 'exit' to quit):\n")
     if message.lower() == "exit":
         break
+    # TODO: implement an `elif message.lower() == "help":` that allows user to then type actions that bypass AI, including: 1. 'refresh' action which causes chat to be re-initialized in case token count is getting too high (i.e., by calling `chat = model.start_chat(...); continue`)
 
     log_message(f"User: {message}")
+    if n_calls == 0:
+        message = message + ai_reminders
 
     response = chat.send_message(message)
-    n_calls+=1
+    n_calls += 1
 
     if not enable_automatic_function_calling:
         finished = False
@@ -82,13 +80,13 @@ while True:
             for part in response.parts:
                 if fn := part.function_call:
                     args = ", ".join(f"{key}={val}" for key, val in fn.args.items())
-                    print(f"\nFunction requested by AI: {fn.name}({args})")
+                    print(f"\nFunction requested by AI: \033[94m{fn.name}\033[0m({args})")
 
                     if fn.name not in TOOLNAMES_SUBSET_HUMAN_NOT_REQUIRED:
                         user_input = input("Do you want to allow this code to execute? (yes/no): ")
                         if user_input.lower() != "yes":
                             result = "Access denied. User blocked function from running."
-                            print("Function not called.")
+                            print("\033[91mFunction not called.\033[0m")
                             log_message(f"AI: Function {fn.name} not called (user denied). ")
                             response_parts_fn.append(genai.protos.Part(
                                 function_response=genai.protos.FunctionResponse(name=fn.name, response={"result": result})
@@ -97,32 +95,40 @@ while True:
 
                     try:
                         result = locals()[fn.name](**fn.args)
-                        print(f"Function '{fn.name}' called successfully.")
-                        log_message(f"AI: Called function {fn.name} with result: {result}")
+                        print(f"\033[92mFunction '{fn.name}' called successfully.\033[0m")
+                        log_message(f"AI: Called function {fn.name}")
 
                     except Exception as e:
                         result = f"Error executing function '{fn.name}': {e}"
-                        print(result)
+                        print(f"\033[91m{result}\033[0m")
                         log_message(f"AI: Error calling function {fn.name}: {e}")
 
                     response_parts_fn.append(genai.protos.Part(
                         function_response=genai.protos.FunctionResponse(name=fn.name, response={"result": result})
                     ))
+
                 if tx := part.text:
-                    print(tx)
-                    log_message(f"AI: {tx.text[:100]}..")
-                    print("\nNOTE: Remember to routinely look over any changes and commit or discard them.\n")
+                    print(f"\033[93m{tx}\033[0m")
+                    log_message(f"AI: {tx[:100]}..")
+                    print("\n\033[95mNOTE: Remember to routinely look over any changes and commit or discard them.\033[0m\n")
 
             if response_parts_fn:
-                print("Returning information from function call/s to AI")
+                print("\033[96mReturning information from function call/s to AI\033[0m")
                 response = chat.send_message(response_parts_fn)
-                n_calls+=1
+                # TODO: Troubleshoot below error. Could it have to do with the escape characters now added to this file for the color-coding?
+                # Traceback (most recent call last):
+                # File "<string>", line 1, in <module>
+                # File "c:\Users\decandia_te\AppData\Local\miniconda3\envs\wcp\lib\site-packages\google\generativeai\generative_models.py", line 588, in send_message
+                #     self._check_response(response=response, stream=stream)
+                # File "c:\Users\decandia_te\AppData\Local\miniconda3\envs\wcp\lib\site-packages\google\generativeai\generative_models.py", line 616, in _check_response
+                #     raise generation_types.StopCandidateException(response.candidates[0])
+                # google.generativeai.types.generation_types.StopCandidateException: finish_reason: MALFORMED_FUNCTION_CALL
+                n_calls += 1
                 if (len(response.parts) == 1) & (response.parts[0].function_call.name == ''):
                     finished = True
             else:
                 finished = True
 
-    else:
-        print(response.text)
-        log_message(f"AI: {response.text[:100]}..")
-        print("\nNOTE: Remember to routinely look over any changes and commit or discard them.\n")
+    print(f"\033[93m{response.text}\033[0m")
+    log_message(f"AI: {response.text[:100]}..")
+    print("\n\033[95mNOTE: Remember to routinely look over any changes and commit or discard them.\033[0m\n")
